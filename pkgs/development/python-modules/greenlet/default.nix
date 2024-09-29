790335e2f984de8c2197e59191dcf79ab7e41bf3
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:04:48 +0200

    python3Packages.greenlet: 3.0.3 -> 3.1.1
    https://github.com/python-greenlet/greenlet/blob/3.1.1/CHANGES.rst

--- pkgs/development/python-modules/greenlet/default.nix
+++ pkgs/development/python-modules/greenlet/default.nix
@@ -16,12 +16,12 @@
 let
   greenlet = buildPythonPackage rec {
     pname = "greenlet";
-    version = "3.0.3";
+    version = "3.1.1";
     pyproject = true;
 
     src = fetchPypi {
       inherit pname version;
-      hash = "sha256-QzdEQjUyWVVM4zWZ2otpLVqpb4l21WfUut8mM3H75JE=";
+      hash = "sha256-TOOsbNtq33lGR11+8xd3wm2UvMw3fgcKeYa9LVxRVGc=";
     };
 
     patches = [
