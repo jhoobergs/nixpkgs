2bf1e824b3b693c33565b0ca3f302f5291e2b58c
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:06:22 +0200

    python3Packages.pynamodb: 6.0.0 -> 6.0.1
    https://github.com/pynamodb/PynamoDB/releases/tag/6.0.1

--- pkgs/development/python-modules/pynamodb/default.nix
+++ pkgs/development/python-modules/pynamodb/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "pynamodb";
-  version = "6.0.0";
+  version = "6.0.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "pynamodb";
     repo = "PynamoDB";
     rev = "refs/tags/${version}";
-    hash = "sha256-Ag/ivZ2SDYX0kwXbExt3kE/pMJgfoGc6gWoy+Rr6GTw=";
+    hash = "sha256-OcrES+1F95KjhRXpEukzbuDfTXU4hyJqxGjD1xMcdKE=";
   };
 
   build-system = [ setuptools ];
