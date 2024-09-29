baacc8b9dd98363b302d6a053cea15e756152ff1
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-30 01:08:31 +0200

    fixup! python3Packages.sphinx: 7.3.7 -> 7.4.7

--- pkgs/development/python-modules/sphinx/default.nix
+++ pkgs/development/python-modules/sphinx/default.nix
@@ -4,7 +4,6 @@
   pythonAtLeast,
   pythonOlder,
   fetchFromGitHub,
-  fetchpatch2,
   isPyPy,
 
   # build-system
@@ -29,6 +28,7 @@
   sphinxcontrib-serializinghtml,
   sphinxcontrib-websupport,
   tomli,
+  typing-extensions,
 
   # check phase
   defusedxml,
@@ -41,7 +41,8 @@
 buildPythonPackage rec {
   pname = "sphinx";
   version = "7.4.7";
-  format = "pyproject";
+  pyproject = true;
+
   disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
@@ -55,16 +56,8 @@ buildPythonPackage rec {
       mv tests/roots/test-images/{testimäge,testimæge}.png
       sed -i 's/testimäge/testimæge/g' tests/{test_build*.py,roots/test-images/index.rst}
     '';
-    hash = "sha256-/lRPYhQrFZ+GhDd1X2/59cUbxnt/MEQfO2p4/APduNg=";
+    hash = "sha256-/5zH9IdLmTGnn5MY4FFSuZOIeF/x1L9Ga/wp57XrAQo=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      name = "python-3.13-compat.patch";
-      url = "https://github.com/sphinx-doc/sphinx/commit/3496de62b743942115acb486cf35dfcc102586c3.patch";
-      hash = "sha256-5VBPOQeGyj3a8VBq4hc9S/eKaeVZeGCTNislwgsauZo=";
-    })
-  ];
 
   build-system = [ flit-core ];
 
@@ -99,6 +92,7 @@ buildPythonPackage rec {
     html5lib
     pytestCheckHook
     pytest-xdist
+    typing-extensions
   ];
 
   preCheck = ''
@@ -121,6 +115,8 @@ buildPythonPackage rec {
       "test_class_alias_having_doccomment"
       "test_class_alias_for_imported_object_having_doccomment"
       "test_decorators"
+      # Assertion error
+      "test_gettext_literalblock_additional"
       # requires cython_0, but fails miserably on 3.11
       "test_cython"
       # Could not fetch remote image: http://localhost:7777/sphinx.png
