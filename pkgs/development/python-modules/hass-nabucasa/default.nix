be53121674e4e96a344d72ff8d13fae1e01c7564
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:04:53 +0200

    python3Packages.hass-nabucasa: 0.81.1 -> 0.82.0
    https://github.com/NabuCasa/hass-nabucasa/releases/tag/0.82.0

--- pkgs/development/python-modules/hass-nabucasa/default.nix
+++ pkgs/development/python-modules/hass-nabucasa/default.nix
@@ -23,7 +23,7 @@
 
 buildPythonPackage rec {
   pname = "hass-nabucasa";
-  version = "0.81.1";
+  version = "0.82.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -32,7 +32,7 @@ buildPythonPackage rec {
     owner = "nabucasa";
     repo = "hass-nabucasa";
     rev = "refs/tags/${version}";
-    hash = "sha256-/sY/JijBCcGcbMjoX0yuhFIWvU+TFVN8sRxBx+CDVVs=";
+    hash = "sha256-hRhRXpiIPrI3umOhsVWLwkSwtEfwevC3fNvJElhKy+I=";
   };
 
   patches = [
