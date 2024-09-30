d2fa35cbcd6a67a82df360206e37bc2a3e1ca13d
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-30 02:55:58 +0200

    fixup! python3Packages.webtest: 3.0.0 -> 3.0.1

--- pkgs/development/python-modules/webtest/default.nix
+++ pkgs/development/python-modules/webtest/default.nix
@@ -3,7 +3,6 @@
   beautifulsoup4,
   buildPythonPackage,
   fetchPypi,
-  fetchpatch,
   setuptools,
   pastedeploy,
   pyquery,
@@ -26,15 +25,6 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-STtcgC+JSKZbXjoa1bJSTuXhq2DNcT2aPaO42ggsBv4=";
   };
-
-  patches = [
-    (fetchpatch {
-      # Replace deprecated unittest aliases for Python 3.12
-      name = "webtest-python312-compat.patch";
-      url = "https://github.com/Pylons/webtest/commit/d82ec5bd2cf3c7109a1d49ad9fa802ae1eae1763.patch";
-      hash = "sha256-hSwxAxAI3Eo28I8S+r2k/hFG8TlzrVYup3MuTsE+xXk=";
-    })
-  ];
 
   build-system = [ setuptools ];
 
