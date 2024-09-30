d8aa1ae03120fa11c6a6a7944af0715909081806
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-30 18:42:04 +0200

    fixup! python3Packages.jaraco-stream: 3.0.3 -> 3.0.4

--- pkgs/development/python-modules/jaraco-stream/default.nix
+++ pkgs/development/python-modules/jaraco-stream/default.nix
@@ -1,24 +1,42 @@
 {
+  lib,
   buildPythonPackage,
   fetchPypi,
+  more-itertools,
+  pytestCheckHook,
+  pythonOlder,
   setuptools-scm,
-  six,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "jaraco-stream";
   version = "3.0.4";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
-    pname = "jaraco.stream";
+    pname = "jaraco_stream";
     inherit version;
     sha256 = "sha256-4rxQKOch7SzIUrluyaM/K3Zk6bLb+H7vvmF9EmZBk0s=";
   };
 
+  build-system = [ setuptools-scm ];
+
+  propagatedBuildInputs = [ more-itertools ];
+
   pythonNamespaces = [ "jaraco" ];
 
-  doCheck = false;
-  buildInputs = [ setuptools-scm ];
-  propagatedBuildInputs = [ six ];
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "jaraco.stream" ];
+
+  meta = with lib; {
+    description = "Module with routines for handling streaming data";
+    homepage = "https://github.com/jaraco/jaraco.stream";
+    changelog = "https://github.com/jaraco/jaraco.stream/blob/v${version}/NEWS.rst";
+    license = licenses.mit;
+    maintainers = [ ];
+  };
 }
