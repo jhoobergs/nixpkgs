1af8c682a682fa4ab64a938b19c311628fb8e88a
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-30 14:35:08 +0200

    fixup! python3Packages.pathvalidate: 3.2.0 -> 3.2.1

--- pkgs/development/python-modules/pathvalidate/default.nix
+++ pkgs/development/python-modules/pathvalidate/default.nix
@@ -3,13 +3,13 @@
   buildPythonPackage,
   fetchPypi,
   pythonOlder,
-  setuptools,
+  setuptools-scm,
 }:
 
 buildPythonPackage rec {
   pname = "pathvalidate";
   version = "3.2.1";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     hash = "sha256-9dB7HiN0GHBAYSofzSvLKRn42xgN8lTJWBu5C/kDN30=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools-scm ];
 
   # Requires `pytest-md-report`, causing infinite recursion.
   doCheck = false;
