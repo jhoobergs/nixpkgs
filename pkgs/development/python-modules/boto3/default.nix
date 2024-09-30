40c4ef7d0270b38afff200571ce9695d832f124c
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-30 11:44:24 +0200

    python312Packages.boto3: 1.34.131 -> 1.35.29

--- pkgs/development/python-modules/boto3/default.nix
+++ pkgs/development/python-modules/boto3/default.nix
@@ -22,16 +22,14 @@ buildPythonPackage rec {
     owner = "boto";
     repo = "boto3";
     rev = "refs/tags/${version}";
-    hash = "sha256-fiUguOzNF9T3CcGD1mYl2b5QFbvBG8wNOd3Or2NR66E=";
+    hash = "sha256-4WP5E8LuuxWZi8DK8yOpvyy6isSfB4eFcbctkTEd3As=";
   };
 
-  nativeBuildInputs = [
-    setuptools
-  ];
+  build-system = [ setuptools ];
 
   pythonRelaxDeps = [ "s3transfer" ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     botocore
     jmespath
     s3transfer
