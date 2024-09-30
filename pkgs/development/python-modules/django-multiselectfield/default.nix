3d09765900a23a0acf2a74f0d2aeb868cf296035
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-30 14:29:17 +0200

    fixup! python3Packages.django-multiselectfield: 0.1.12 -> 0.1.13

--- pkgs/development/python-modules/django-multiselectfield/default.nix
+++ pkgs/development/python-modules/django-multiselectfield/default.nix
@@ -3,26 +3,37 @@
   buildPythonPackage,
   fetchPypi,
   django,
+  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "django-multiselectfield";
   version = "0.1.13";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.9";
 
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-Q31yYy9MDKQWlRkXYyUpw9HUK2K7bDwD4zlvpQJlvpQ=";
+    pname = "django_multiselectfield";
+    inherit version;
+    hash = "sha256-Q31yYy9MDKQWlRkXYyUpw9HUK2K7bDwD4zlvpQJlvpQ=";
   };
 
-  propagatedBuildInputs = [ django ];
+  build-system = [ setuptools ];
+
+  dependencies = [ django ];
 
   # No tests
   doCheck = false;
 
+  pythonImportsCheck = [ "multiselectfield" ];
+
   meta = {
-    description = "django-multiselectfield";
+    description = "Multiple Choice model field for Django";
     homepage = "https://github.com/goinnn/django-multiselectfield";
-    license = lib.licenses.lgpl3;
+    changelog = "https://github.com/goinnn/django-multiselectfield/blob/master/CHANGES.rst";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = [ ];
   };
 }
